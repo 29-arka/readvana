7b3a4cf223689617aef7f6fd1375f82cb109643d
29-arka <arkachakraborty468@gmail.com>
2025-08-13 14:23:42 +0530

    commit four

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -6,11 +6,11 @@ const ProductItem = ({id,image,name,price}) => {
     const {currency} = useContext(ShopContext);
   return (
     <Link className='text-gray-700 cursor pointer'to={`/product/${id}`}>
-      <div className='overflow-hidden w-48 h-68'>
-        <img className='object-contain w-full h-full hover:scale-110 transition ease-in-out' src={image[0]} alt=""/>
+      <div className='overflow-hidden'>
+        <img className='object-contain w-48 h-68 hover:scale-110 transition ease-in-out' src={image[0]} alt=""/>
+        <p className='pt-3 pb-1 text-sm'>{name}</p>
+        <p className='text-sm font-medium'>{currency}{price}</p>
       </div>
-      <p className='pt-3 pb-1 tex-sm'>{name}</p>
-      <p className='tex-sm front-medium'>{currency}{price}</p>
     </Link>
   )
 }
